a82f396b8c4dcd002cdbbaeabef748317038114a
Matheraptor <hamedahastral@gmail.com>
2026-05-15 11:46:12 +0200

    0.22.6 throttle_node.sh v2

--- throttle_node.sh
+++ throttle_node.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+cd /home/hamedahastral/MAGPIE_Server
+
 CPU_LIMIT=30
 
 while true; do
-  for NODE_PID in $(pgrep -x node); do
-    # Check if a cpulimit process is already targeting THIS specific Node PID
-    if ! ps aux | grep "cpulimit" | grep -q " -p $NODE_PID"; then
-      cpulimit -p $NODE_PID -l $CPU_LIMIT -b
+    echo -e "\e[36m--- Launching MAGPIE Server Core... ---\e[0m"
+    
+    # 1. Run Node natively in the background
+    node SERVER.js &
+    NODE_PID=$!
+    
+    # 2. Immediately throttle this exact process ID to 30% CPU
+    cpulimit -p $NODE_PID -l $CPU_LIMIT -b
+    
+    # 3. Wait for this specific Node instance to exit or crash
+    wait $NODE_PID
+    exitCode=$?
+    
+    # 4. Evaluate exit codes
+    if [ "$exitCode" -eq 2 ]; then
+        echo -e "\e[36m--- Restart signal received CODE[2]: rebooting... ---\e[0m"
+    elif [ "$exitCode" -eq 0 ]; then
+        echo -e "\e[32m--- Server shut down normally CODE[0]. Exiting wrapper loop. ---\e[0m"
+        break
+    else
+        echo -e "\e[31m--- Server crashed or stopped CODE[$exitCode]. Graceful pause... ---\e[0m"
+        sleep 2
     fi
-  done
-  sleep 10
 done
