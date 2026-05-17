075a2f017145f55db5c321fce0e9c3c1a9d4e045
matheraptor <hamedahastral@gmail.com>
2026-05-17 09:57:12 +0000

    fix(remote): ./start.sh

--- run_server.sh
+++ run_server.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-
 while true; do
-    # Run server
     node SERVER.js
+    EXIT_CODE=$?
     
-    # Capture exit code
-    exitCode=$?
-    
-    # Decide what to do
-    if [ "$exitCode" -eq 2 ]; then
+    if [ $EXIT_CODE -eq 2 ]; then
         echo -e "\e[36m--- Restart signal received CODE[2]: rebooting MAGPIE... ---\e[0m"
-    elif [ "$exitCode" -eq 0 ]; then
+        sleep 1
+    elif [ $EXIT_CODE -eq 0 ]; then
         echo -e "\e[32m--- Server shut down normally CODE[0]. Exiting loop. ---\e[0m"
         break
     else
-        echo -e "\e[31m--- Server crashed or stopped CODE[$exitCode]. ---\e[0m"
+        echo -e "\e[31m--- Server crashed or stopped CODE[$EXIT_CODE]. ---\e[0m"
         break
     fi
 done
