8798c15ed6912ef7b81c7613e10a314616e68033
Matheraptor <42889806+matheraptor@users.noreply.github.com>
2026-06-11 01:50:28 +0200

    0.38.6 - Update SERVER.js

--- SERVER.js
+++ SERVER.js
@@ -1618,7 +1618,7 @@ instrument(io, {
 	mode: "development"
 })
 app.use((req, res, next) => {
-	console.log(`[HTTP REQUEST] ${req.method} ${req.url}`)
+	console.log(`${MAGPIE_SYSTEM.Utility.CTZF()} [HTTP REQUEST] ${req.method} ${req.url}`)
 	next()
 })
 app.use(express.static("./public"));
